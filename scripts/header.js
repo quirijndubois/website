af815c579faf2ca3c547fb538d2e2a3c7ad8a114
Quirijn du Bois <quirijndubois@gmail.com>
2025-01-03 00:55:40 +0100

    Some pretty animations

--- scripts/header.js
+++ scripts/header.js
@@ -4,6 +4,7 @@ function updateHeader() {
     window.requestAnimationFrame(updateHeader);
 
     const header = document.querySelector('.header');
+    const links = document.querySelector('.links');
 
     headerHeight = header.getBoundingClientRect().top;
 
@@ -16,4 +17,7 @@ function updateHeader() {
 
     let scale = mapRangeClamp(window.scrollY, window.innerHeight - headerOffset * 2, window.innerHeight / 2 - headerOffset * 2, 0.5, 1);
     header.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
+
+    let opacity = mapRangeClamp(window.scrollY, window.innerHeight - headerOffset * 2, window.innerHeight / 2 - headerOffset * 2, 0, 1);
+    links.style.opacity = opacity;
 }
