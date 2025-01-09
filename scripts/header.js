935060504ce03aef623335740f2919f7a9085ec4
Quirijn du Bois <quirijndubois@gmail.com>
2025-01-09 22:55:21 +0100

    Minor fixed

--- scripts/header.js
+++ scripts/header.js
@@ -4,7 +4,6 @@ function updateHeader() {
     window.requestAnimationFrame(updateHeader);
 
     const header = document.querySelector('.header');
-    const links = document.querySelector('.links');
 
     headerHeight = header.getBoundingClientRect().top;
 
@@ -17,7 +16,4 @@ function updateHeader() {
 
     let scale = mapRangeClamp(window.scrollY, window.innerHeight - headerOffset * 2, window.innerHeight / 2 - headerOffset * 2, 0.5, 1);
     header.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
-
-    let opacity = mapRangeClamp(window.scrollY, window.innerHeight * 0.5, window.innerHeight * 0.3, 0, 1);
-    links.style.opacity = opacity;
 }
